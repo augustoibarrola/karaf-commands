4cc01452f4a5849d0cfc7edf13951116a44df119
augustoibarrola <augusto.t.ibarrola@gmail.com>
2022-05-10 16:52:32 -0400

    deleted old command and created new one from cookbook

---
+++ com.geryon.math/src/main/java/com/geryon/commands/add.java
@@ -0,0 +1,27 @@
+
+package com.geryon.commands;
+
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Argument;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.Option;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+
+@Command(scope = "math", name = "add", description = "adds simple whole integers ")
+@Service
+public class add implements Action {
+
+    @Option(name = "-o", aliases = { "--option" }, description = "An option to the command", required = false, multiValued = false)
+    private String option;
+
+    @Argument(name = "argument", description = "Argument to the command", required = false, multiValued = false)
+    private String argument;
+
+    @Override
+    public Object execute() throws Exception {
+         System.out.println("Executing command add");
+         System.out.println("Option: " + option);
+         System.out.println("Argument: " + argument);
+         return null;
+    }
+}
