56da373a36be40db12282e75a16f743c081c4cdc
augustoibarrola <augusto.t.ibarrola@gmail.com>
2022-05-11 23:07:14 -0400

    math:add command lists digits given

--- Math/src/main/java/com/geryon/Add.java
+++ Math/src/main/java/com/geryon/Add.java
@@ -1,6 +1,8 @@
 
 package com.geryon;
 
+import java.util.List;
+
 import org.apache.karaf.shell.api.action.Action;
 import org.apache.karaf.shell.api.action.Argument;
 import org.apache.karaf.shell.api.action.Command;
@@ -14,14 +16,18 @@ public class Add implements Action {
     @Option(name = "-o", aliases = { "--option" }, description = "An option to the command", required = false, multiValued = false)
     private String option;
 
-    @Argument(name = "argument", description = "Argument to the command", required = false, multiValued = false)
-    private String argument;
+    @Argument(name = "-d", description = "Argument to the command", required = false, multiValued = true)
+    private List<Integer> digits;
 
     @Override
     public Object execute() throws Exception {
-         System.out.println("Executing command add");
-         System.out.println("Option: " + option);
-         System.out.println("Argument: " + argument);
+        //  System.out.println("Executing command add");
+        //  System.out.println("Option: " + option);
+         System.out.println("Digits: \n");
+         for(Integer digit:digits)
+         {
+            System.out.print(digit + "\n");
+         }
          return null;
     }
 }
